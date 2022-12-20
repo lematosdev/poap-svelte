e57473368304687d2f64acff3a0a255d9a76fa10
macnemoto <marcosj465@gmail.com>
2022-12-19 21:57:08 -0400

    :tada: tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,15 @@
 
 <main>
   <div>
+    <h1>hola soy tailwid</h1>
+    <span class="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+      Hello<br>
+      World
+    </span>
+    <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+      Hello<br>
+      World
+    </span>
     <h1>Graficos POAP</h1>
     <p>Estos son los graficos de los POAP</p>
   </div>
