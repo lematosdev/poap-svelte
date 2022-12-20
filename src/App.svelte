0c805ec279968ca62e2d6d51415a00d112fabd2f
Carlos Matos <lematosdev@gmail.com>
2022-12-19 21:22:29 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Pie from './lib/Pie.svelte';
+</script>
+
+<main>
+  <div>
+    <h1>Graficos POAP</h1>
+    <p>Estos son los graficos de los POAP</p>
+  </div>
+  <Pie />
+</main>
---
+++ src/lib/data.ts
@@ -0,0 +1,12 @@
+export const data = {
+  labels: ['Virtual', 'Physical'],
+  datasets: [
+    {
+      data: [41, 70],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)'
+      ]
+    }
+  ]
+};
